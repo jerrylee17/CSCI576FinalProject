10e606b2bb99247baa8f7de66677dd801e2056ff
tommyshang <56244127+tommyshang@users.noreply.github.com>
2022-11-18 20:35:11 -0800

    Fix motion vector bugs

--- objects/block.py
+++ objects/block.py
@@ -26,9 +26,10 @@ class Block:
   def calculate_motion_vector(self, previous_frame_data: List[List[List[int]]]) -> None:
     """Calculates motion vector based on MAD"""
     # Convert to hsv
-    self.convert_to_hsv()
-    min_MAD: float = sys.float_info.max
 
+    self.convert_to_hsv()
+    self.frame_convert_to_hsv(previous_frame_data)
+    min_MAD: float = sys.float_info.max
     for i in range(self.position[0] - MACRO_SIZE, self.position[0] + MACRO_SIZE, 1):
       for j in range(self.position[1] - MACRO_SIZE, self.position[1] + MACRO_SIZE, 1):
         if self.isValid(i, j, previous_frame_data):
@@ -37,6 +38,8 @@ class Block:
           if MAD < min_MAD:
             min_MAD = MAD
             self.vector = (i-self.position[0], j-self.position[1])
+    #print(self.vector)
+    #print(min_MAD)
 
   def calculate_block_MAD(self, previous_frame_block: list[list[list[int, int, int]]]) -> int:
     sum: float = 0
@@ -49,7 +52,7 @@ class Block:
   #     return np.mean(np.abs(self.data - previous_frame_block))[0]
 
   def isValid(self, x: int, y: int, previous_frame_data: list[list[list[int, int, int]]]) -> bool:
-    return 0 <= x < len(previous_frame_data) - MACRO_SIZE and 0 <= y < len(
+    return 0 <= x <= len(previous_frame_data) - MACRO_SIZE and 0 <= y <= len(
       previous_frame_data[0]) - MACRO_SIZE
 
   def convert_to_hsv(self) -> None:
@@ -62,6 +65,12 @@ class Block:
         self.data[x][y] = rgb_to_hsv(r, g, b)
     self.data_type = "hsv"
 
+  def frame_convert_to_hsv(self,frame_data:List[List[List[int]]]) -> None:
+    for x in range(len(frame_data)):
+      for y in range(len(frame_data[0])):
+        r, g, b = frame_data[x][y]
+        frame_data[x][y] = rgb_to_hsv(r, g, b)
+
   def convert_to_rgb(self) -> None:
     """Convert pixels to RGB"""
     if self.data_type == "rgb":
@@ -71,3 +80,57 @@ class Block:
         r, g, b = self.data[x][y]
         self.data[x][y] = hsv_to_rgb(r, g, b)
     self.data_type = "rgb"
+
+def shift_to_right_by_one():
+  original_data = np.random.rand(MACRO_SIZE,MACRO_SIZE,3)
+  data1 = np.random.rand(MACRO_SIZE,1,3)
+  frame =np.concatenate((data1, original_data), axis=1)
+
+  my_block = Block(original_data,1,(0,0))
+  my_block.calculate_motion_vector(frame)
+
+def shift_up_by_one():
+  original_data = np.random.rand(MACRO_SIZE,MACRO_SIZE,3)
+  data1 = np.random.rand(1,MACRO_SIZE,3)
+  frame =np.concatenate((data1, original_data), axis=0)
+
+  my_block = Block(original_data,1,(0,0))
+  my_block.calculate_motion_vector(frame)
+
+def shift_down_by_one():
+  original_data = np.random.rand(MACRO_SIZE,MACRO_SIZE,3)
+  my_block = Block(original_data, 1, (1, 0))
+
+  data1 = np.random.rand(1,MACRO_SIZE,3)
+  frame = np.concatenate((original_data, data1), axis=0)
+
+  my_block.calculate_motion_vector(frame)
+
+def not_0_0_block():
+  original_data = np.random.rand(MACRO_SIZE, MACRO_SIZE, 3)
+  my_block = Block(original_data, 1, (10, 10))
+  frame = np.random.rand(100, 100, 3)
+  index = (3,3)
+  frame[index[0]:index[0] + MACRO_SIZE, index[1]: index[1] + MACRO_SIZE] = original_data
+  my_block.calculate_motion_vector(frame)
+
+def not_same_data():
+
+  original_data = np.ones((MACRO_SIZE, MACRO_SIZE, 3)) * 10
+  my_block = Block(original_data, 1, (0, 0))
+  frame = np.ones((100, 100, 3)) * 15
+  add = np.ones((MACRO_SIZE, MACRO_SIZE, 3)) * 11
+
+  index = (20, 11)
+  frame[index[0]:index[0] + MACRO_SIZE, index[1]: index[1] + MACRO_SIZE] = add
+  my_block.calculate_motion_vector(frame)
+
+if __name__ == '__main__':
+  #(0,1)
+  shift_to_right_by_one()
+  # (1,0)
+  shift_up_by_one()
+  #(-1, 0)
+  shift_down_by_one()
+  #(3,3)
+  not_0_0_block()
--- test/test.py
+++ test/test.py
@@ -1,9 +1,9 @@
 from util.io import read_video, display_video
 
-
 def test_video():
     video = read_video("../videos/SAL_490_270_437")
     display_video(video)
+
 
 
 if __name__ == '__main__':
